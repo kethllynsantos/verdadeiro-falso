21ce7bf96782000f1df1a745b717b9f6cfa26dc5
kethllynsantos <kethllyn.santos@escola.pr.gov.br>
2026-03-20 12:23:50 -0300

    salve

---
+++ verdadeiro-falso.js
@@ -0,0 +1,23 @@
+// booleano
+
+const usuariologado = true;
+const contapaga = false;
+
+// falso e verdadeiro
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+  // undetined 
+
+  let minhavar;
+  let VaeNu11 = null;
+
+  let numero = 3;
+  let texto = "alura";
+
+  console.log(typeof numero);
+  console.log(typeof texto);
+
+  
