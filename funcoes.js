2c4a46185d5a76f28c6198ade81ec9e6fca96d75
kethllynsantos <kethllyn.santos@escola.pr.gov.br>
2026-05-21 12:16:01 -0300

    vjb,

---
+++ funcoes.js
@@ -0,0 +1,10 @@
+console.log("--- INICIO DO TESTE DA TURMA");
+console.log("");// linha em branco para organizar
+//
+function mostrarMensagem(texto){
+    console.log( exemplo1 -> mensagem recebida; + texto);
+}
+
+mostrarMensagem(Bem-vindos ao GitHub codespaces!);
+mostrarMensagem(""); //linha em branco pra organizar
+
